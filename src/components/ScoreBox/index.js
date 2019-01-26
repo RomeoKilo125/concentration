169ac4be2b2b2a45d9112e6ba5584db4210b3dcd
RomeoKilo125 <romeokilo125@gmail.com>
2019-01-25 23:12:46 -0500

    properly show score

--- src/components/ScoreBox/index.js
+++ src/components/ScoreBox/index.js
@@ -2,13 +2,10 @@ import React, {Component} from "react"
 import './style.css'
 
 class ScoreBox extends Component {
-  state = {
-    score: this.props.score
-  }
 
   render() {
     return (
-      <div>Score: {this.state.score}</div>
+      <div>Score: {this.props.score}</div>
     )
   }
 }
